d165c668c4a799b77afd228b4a13ce35dc7a94ff
Adam Sanderson <netghost@gmail.com>
2009-05-26 16:56:05 -0700

    Submit on ctrl-enter.

--- tread.rb
+++ tread.rb
@@ -1,3 +1,5 @@
+CTRL_ENTER = "\342\200\250" # ctrl-enter (on a mac anyways...)
+
 Shoes.app do
     background "#ddd"
     style(Para, :font => "Monospace 12px")
@@ -6,7 +8,12 @@ Shoes.app do
       para "Welcome to tread"
     end
     flow do 
-      @input = edit_line(:width=> -60)
+      @input = edit_box(:height=>50, :width=> -60) do |input|
+        if input.text =~ /#{CTRL_ENTER}\Z/ 
+          run_command(input.text.chomp(CTRL_ENTER))
+        end
+      end
+      
       button("run"){
         run_command(@input.text)
       }
