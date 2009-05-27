1309f201c90e273241ec6804ebfc892efd4ab4c9
Adam Sanderson <netghost@gmail.com>
2009-05-26 17:42:32 -0700

    Making it a little nicer on the eyes

--- tread.rb
+++ tread.rb
@@ -6,18 +6,23 @@ Shoes.app do
     
     @exp_checker = ExpressionChecker.new
     
-    @console = stack :width => 1.0, :height => -50, :scroll => true do
-      para "Welcome to tread"
+    @header = stack :height=>48, :width=>1.0 do
+      background "#fff".."#ddd"
+      flow do
+        tagline "Welcome to Tread", :font=>"SanSerif"
+        # ... add some links for twiddling things here
+      end
     end
+    
+    @console = stack :width => 1.0, :height => -96, :scroll => true
+
     flow do 
       @input = edit_box(:height=>50, :width=> -60) do |input|
         text = input.text
         run_command(text.chomp("\n")) if text =~ /\n\Z/ && @exp_checker.valid?(text)
       end
       
-      button("run"){
-        run_command(@input.text)
-      }
+      button("run"){ run_command(@input.text) }
     end
         
     def run_command(command)
